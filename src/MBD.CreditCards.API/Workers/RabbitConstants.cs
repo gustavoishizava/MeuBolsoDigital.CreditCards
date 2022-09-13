a90e25d248a903288bce42dbe7bc539f059e22b9
Gustavo Cabral Ishizava <gustavo@MacBook-Pro-de-Gustavo.local>
2022-09-12 21:23:50 -0300

    adjust code coverage

--- src/MBD.CreditCards.API/Workers/RabbitConstants.cs
+++ src/MBD.CreditCards.API/Workers/RabbitConstants.cs
@@ -1,5 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace MBD.CreditCards.API.Workers
 {
+    [ExcludeFromCodeCoverage]
     public static class RabbitConstants
     {
         public const string BANK_ACCOUNT_CREATED = "TOPIC/bank_accounts.created";
